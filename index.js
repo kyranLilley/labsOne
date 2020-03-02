81a8224fa1c32352f4c5005529c3540c3bfa8cbd
kyran lilley <kyran-lilley@live.nmit.ac.nz>
2020-03-02 15:08:46 +1300

    update index

---
+++ index.js
@@ -0,0 +1,29 @@
+let thisVar = 50;
+let firstName = "kyran";
+let lastname = "lilley";
+var numberAddAgain = 1;
+var anotherFirstName
+
+if (thisVar == 50){
+    this.numberAddAgain++;
+console.log(firstName + " " + lastname);
+//addOne();
+//return numberAddAgain;
+};
+
+// function addOne(){
+//     let numberAddAgain = numberAddAgain + 1;
+// }
+
+console.log(numberAddAgain);
+
+
+var thisNumber = 5;
+var thisOtherNumber = 5;
+var sum = thisNumber + thisOtherNumber;
+var sub = thisNumber - thisOtherNumber;
+var div = thisNumber / thisOtherNumber;
+console.log("sum = " + sum)
+console.log("sub = " + sub)
+console.log("div = " + div)
+
